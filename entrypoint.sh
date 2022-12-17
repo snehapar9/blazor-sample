ba6f0a986731bdd43709058c833f01a8666d68bf
Sneha Parthasarathy <snehapar@microsoft.com>
2022-12-16 20:17:57 -0800

    Add time seconds

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh -l
 echo "Starting Oryx build"
+secs=$SECONDS
 oryx build /github/workspace/$1
+hrs=$(( secs/3600 )); mins=$(( (secs-hrs*3600)/60 )); secs=$(( secs-hrs*3600-mins*60 ))
 echo "Finishing Oryx build"
