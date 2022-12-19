81e5741519eb88a68a0c3ba1b1af502fc47cbc80
Sneha Parthasarathy <snehapar@microsoft.com>
2022-12-18 23:39:28 -0800

    Modified action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/sh -l
 echo "Starting Oryx build"
-secs=$SECONDS
-#oryx build /github/workspace/$1
-hrs=$(( secs/3600 )); mins=$(( (secs-hrs*3600)/60 )); secs=$(( secs-hrs*3600-mins*60 ))
+oryx build /github/workspace/$1
 echo "Finishing Oryx build"
